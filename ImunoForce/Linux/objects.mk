2b4b59ab6ca78e63cb99bd24df5e587108fe4e53
FelipeSBonetto <felipesensbonetto@gmail.com>
2015-07-03 06:07:07 -0300

    Compile para linux

---
+++ ImunoForce/Linux/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lallegro -lallegro_audio -lallegro_acodec -lallegro_font -lallegro_ttf -lm -lallegro_dialog -lallegro_primitives -lallegro_image
+
